a8b4cadb0fcd8a53122a3e0f7640e8031c983e4f
Aiden Isaia <aidenisaia@gmail.com>
2021-06-20 10:17:32 -0700

    update and destroy

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,4 +20,24 @@ class SongsController < ApplicationController
     song.save
     render json: song.as_json
   end
+
+  def update
+    the_id = params[:id]
+    song = Song.find_by(id: the_id)
+
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+
+    song.save
+    render json: song.as_json
+  end
+
+  def destroy
+    the_id = params[:id]
+    song = Song.find_by(id: the_id)
+    song.delete
+    render json: song.as_json
+  end
 end
