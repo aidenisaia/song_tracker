923b528137ea12a4426a2c377bf9d06ca834d33a
Aiden Isaia <aidenisaia@gmail.com>
2021-06-20 09:59:34 -0700

    create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,21 @@ class SongsController < ApplicationController
     songs = Song.all
     render json: songs.as_json
   end
+
+  def show
+    the_id = params[:id]
+    song = Song.find_by(id: the_id)
+    render json: song.as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    song.save
+    render json: song.as_json
+  end
 end
